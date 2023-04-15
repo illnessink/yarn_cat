c8cf66a41b8bf30b6913ac9605c6968663e4e25b
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-15 14:20:03 -0400

    wip: adds path for update and delete project

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('projects/', views.projects_index, name='projects_index'),
     path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name="project_create"),
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name="project_update"),
+    path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name="project_delete"),
     path('accounts/signup/', views.signup, name='signup')
 ]
 
