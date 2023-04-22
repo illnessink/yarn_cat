e414c0b3fb6c46af1813297aea921e4929befeb2
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-22 17:00:19 -0400

    wip: adds youtube api to settings

--- yarn_cat/settings.py
+++ yarn_cat/settings.py
@@ -134,3 +134,5 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+YOUTUBE_DATA_API_KEY = os.environ['YOUTUBE_DATA_API_KEY']
