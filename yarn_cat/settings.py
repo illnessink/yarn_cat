0f8d1e69a4bf614ef50c79b29644e52e530c89a9
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-25 22:24:26 -0400

    debugging heroku

--- yarn_cat/settings.py
+++ yarn_cat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://git.heroku.com/yarncat.git', 'localhost',]
+ALLOWED_HOSTS = ['https://git.heroku.com/yarncat.git']
 
 
 # Application definition
