a9b1ae6f1420643535e7d1c344fc242a31da2b9d
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-26 23:03:44 -0400

    wip adds paths for favs, unfavs and all favs

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('projects/<int:project_id>/add_photo/', views.add_photo, name='add_photo'),
     path('projects/<int:project_id>/add_timing/', views.add_timing, name='add_timing'),
     path('tutorials/', views.tutorial_search, name='tutorial_search'),
+    path('tutorials/<str:video_id>/favorite/', views.add_favorite_video, name='add_favorite_video'),
+    path('tutorials/<str:video_id>/unfavorite/', views.unfavorite_video, name='unfavorite_video'),
+    path('tutorials/favorite/', views.favorite_videos, name='favorite_videos'),
     path('accounts/signup/', views.signup, name='signup')
 ]
 
