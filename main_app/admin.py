a8e59c48f473b456f2e02c5e5d178edb5f7be5af
Vanessa Foerster <nessfoerster@gmail.com>
2023-04-26 21:41:23 -0400

    adds Video to admins panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Project, Photo, Timing
+from .models import Project, Photo, Timing, Video
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Photo)
 admin.site.register(Timing)
+admin.site.register(Video)
